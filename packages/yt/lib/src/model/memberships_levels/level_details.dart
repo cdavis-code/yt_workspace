740f689de6c8cd315307eb68cfdab022c9b1bae5
cdavis-code <unnghabunga@gmail.com>
2026-05-09 11:21:31 -0400

    feat: add LevelDetails model for MembershipsLevels API

---
+++ packages/yt/lib/src/model/memberships_levels/level_details.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'level_details.g.dart';
+
+@JsonSerializable()
+class LevelDetails {
+  @JsonKey(name: 'displayName')
+  final String displayName;
+
+  LevelDetails({
+    required this.displayName,
+  });
+
+  factory LevelDetails.fromJson(Map<String, dynamic> json) =>
+      _$LevelDetailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LevelDetailsToJson(this);
+}
