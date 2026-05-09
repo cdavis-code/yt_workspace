a182e69bdc2217baed1d55835c99f70de629021a
cdavis-code <unnghabunga@gmail.com>
2026-05-09 11:24:47 -0400

    feat: add MembershipsLevels API class

---
+++ packages/yt/lib/src/memberships_levels.dart
@@ -0,0 +1,17 @@
+import 'package:yt/src/youtube_api_helper.dart';
+import 'package:yt/yt.dart';
+
+import 'provider/data/memberships_levels.dart';
+
+class MembershipsLevels extends YouTubeApiHelper {
+  final MembershipsLevelsClient _rest;
+
+  MembershipsLevels({required super.dio}) : _rest = MembershipsLevelsClient(dio);
+
+  Future<MembershipsLevelListResponse> list({
+    String part = 'id,snippet',
+  }) async => _rest.list(
+    accept,
+    buildParts([], part),
+  );
+}
