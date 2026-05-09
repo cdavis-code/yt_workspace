bdee26eeb2ee232fb204a18483729dc5a96d64f3
cdavis-code <unnghabunga@gmail.com>
2026-05-09 11:21:29 -0400

    feat: add Member model for Members API

---
+++ packages/yt/lib/src/model/members/member.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'member_snippet.dart';
+
+part 'member.g.dart';
+
+@JsonSerializable()
+class Member {
+  final String kind;
+  final String etag;
+  final MemberSnippet snippet;
+
+  Member({
+    this.kind = 'youtube#member',
+    required this.etag,
+    required this.snippet,
+  });
+
+  factory Member.fromJson(Map<String, dynamic> json) =>
+      _$MemberFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MemberToJson(this);
+}
