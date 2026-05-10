6bc9cbf2a5aa28c4ea9ddb10848f0fce6d70c467
cdavis-code <unnghabunga@gmail.com>
2026-05-09 21:21:53 -0400

    feat: add VideoAbuseReportReasonSnippet model for VideoAbuseReportReasons API

---
+++ packages/yt/lib/src/model/video_abuse_report_reasons/video_abuse_report_reason_snippet.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'secondary_reason.dart';
+
+part 'video_abuse_report_reason_snippet.g.dart';
+
+@JsonSerializable()
+class VideoAbuseReportReasonSnippet {
+  final String label;
+
+  @JsonKey(name: 'secondaryReasons')
+  final List<SecondaryReason>? secondaryReasons;
+
+  VideoAbuseReportReasonSnippet({
+    required this.label,
+    this.secondaryReasons,
+  });
+
+  factory VideoAbuseReportReasonSnippet.fromJson(Map<String, dynamic> json) =>
+      _$VideoAbuseReportReasonSnippetFromJson(json);
+
+  Map<String, dynamic> toJson() => _$VideoAbuseReportReasonSnippetToJson(this);
+}
