8819b168d9116bf4ad48578f459a5767c857791e
cdavis-code <unnghabunga@gmail.com>
2026-05-10 09:32:20 -0400

    feat: add VideoAbuseReportReasons API class

---
+++ packages/yt/lib/src/video_abuse_report_reasons.dart
@@ -0,0 +1,20 @@
+import 'package:yt/src/youtube_api_helper.dart';
+import 'package:yt/yt.dart';
+
+import 'provider/data/video_abuse_report_reasons.dart';
+
+class VideoAbuseReportReasons extends YouTubeApiHelper {
+  final VideoAbuseReportReasonsClient _rest;
+
+  VideoAbuseReportReasons({required super.dio})
+      : _rest = VideoAbuseReportReasonsClient(dio);
+
+  Future<VideoAbuseReportReasonListResponse> list({
+    String part = 'id,snippet',
+    String? hl,
+  }) async => _rest.list(
+        accept,
+        buildParts([], part),
+        hl: hl,
+      );
+}
