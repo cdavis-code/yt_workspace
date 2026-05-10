d29ade44fdf8816971891ef7939cd6c019df7703
cdavis-code <unnghabunga@gmail.com>
2026-05-10 09:51:17 -0400

    feat: add YoutubeVideoAbuseReportReasonsCommand for CLI

---
+++ packages/yt_cli/lib/src/cmd/youtube_video_abuse_report_reasons_command.dart
@@ -0,0 +1,61 @@
+import 'package:args/command_runner.dart';
+import 'package:dio/dio.dart';
+import 'package:yt/yt.dart';
+
+import 'youtube_helper_command.dart';
+
+class YoutubeVideoAbuseReportReasonsCommand extends Command<void> {
+  @override
+  String get description =>
+      'A videoAbuseReportReason resource contains information about a reason that a video would be flagged for containing abusive content.';
+
+  @override
+  String get name => 'video-abuse-report-reasons';
+
+  YoutubeVideoAbuseReportReasonsCommand() {
+    addSubcommand(YoutubeListVideoAbuseReportReasonsCommand());
+  }
+}
+
+class YoutubeListVideoAbuseReportReasonsCommand extends YtHelperCommand {
+  @override
+  String get description =>
+      'Retrieves a list of reasons that can be used to report abusive videos.';
+
+  @override
+  String get name => 'list';
+
+  YoutubeListVideoAbuseReportReasonsCommand() {
+    argParser
+      ..addOption(
+        'part',
+        defaultsTo: 'id,snippet',
+        help:
+            'The part parameter specifies the videoAbuseReportReason resource parts that the API response will include.',
+      )
+      ..addOption(
+        'hl',
+        valueHelp: 'language',
+        help:
+            'The hl parameter specifies the language that should be used for text values in the API response.',
+      );
+  }
+
+  @override
+  void run() async {
+    await initializeYt();
+
+    try {
+      final response = await videoAbuseReportReasons.list(
+        part: argResults!['part'],
+        hl: argResults?['hl'],
+      );
+
+      print(response);
+
+      close();
+    } on DioException catch (err) {
+      throw UsageException('API usage error:', err.usage);
+    }
+  }
+}
